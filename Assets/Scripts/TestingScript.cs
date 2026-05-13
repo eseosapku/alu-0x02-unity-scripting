fd1882e363e8f6de8c693a12f54e78c65dc83939
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-13 13:03:20 +0200

    commit

--- Assets/Scripts/TestingScript.cs
+++ Assets/Scripts/TestingScript.cs
@@ -5,12 +5,12 @@ public class TestingScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("go");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("continue");
     }
 }
