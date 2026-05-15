e3f3864b8a4f7ffc2757e4059f6e8b3c1fa852de
Eseosa Kay-Uwagboe Pascal <75255976+eseosapku@users.noreply.github.com>
2026-05-15 10:59:43 +0200

    commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    //this is the player movement method
+
+    void PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);        
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.S)) 
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A)) 
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D)) 
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
